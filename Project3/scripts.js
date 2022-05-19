b8c532d137c7138cc4815b2dbd36f3dcb7ec1cc7
Myltiane Alves <myltiane.alves@cleandev.com.br>
2022-05-19 20:58:00 -0300

    Adicionando projeto 02

---
+++ Project3/scripts.js
@@ -0,0 +1,29 @@
+
+const todosApp = {
+    data() {
+        return {
+            todos: [],
+            newTodo: {
+                done: false,
+            }
+        }
+    },
+    methods: {
+        addTodo: function () {
+            if(this.newTodo.text) {
+                this.todos.push(this.newTodo);
+                this.newTodo = {
+                    done: false,
+                };
+                localStorage.setItem('todos', JSON.stringify(this.todos));
+            } else {
+                alert('um texto é necessário');
+            }
+        }
+    },
+    created() {
+        this.todos = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : this.todos;
+    },
+};
+
+Vue.createApp(todosApp).amount('#app');
